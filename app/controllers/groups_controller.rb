e35a4e0e95a30ac35a241102ff9e15013daea670
kana uchimura <xxxxxx@gmail.com>
2019-10-18 12:07:35 +0900

    creaa

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,29 +2,31 @@ class GroupsController < ApplicationController
     def index
     end
     def new
-        @group = group.new
-        @group.users << current_user
+      @group = Group.new
+      @group.users << current_user
     end
+  
     def create
-        if @group.save
-            rediet_to root_path, notice: 'グループを作成しました'
-        else
-            render :new
-        end
+      @group = Group.new(group_params)
+      if @group.save
+        redirect_to root_path, notice: 'グループを作成しました'
+      else
+        render :new
+      end
     end
-    def update
+      def update
         if @group.update(group_params)
           redirect_to group_messages_path(@group), notice: 'グループを編集しました'
         else
           render :edit
         end
     end
+  
     private
     def group_params
-        params.require(:group).permit(:name, { :user_ids => []})
-    end    
-
+      params.require(:group).permit(:name, { :user_ids => [] })
+    end
     def set_group
         @group = Group.find(params[:id])
-      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html	  root to: 'message#index'
   root 'groups#index'
-  resources :messages, only: :index do
-  end
+  resources :messages, only: [:index, :create]
   resources :users, only: [:edit, :update] do
   end
 
