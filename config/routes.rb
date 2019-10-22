6705919babe2f38673239735c2c2cb5c0fb3d844
kana uchimura <xxxxxx@gmail.com>
2019-10-22 19:11:16 +0900

    インタクリメンタルサーチ

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  get 'users/edit'
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html	  root to: 'message#index'
- 
-  
-  resources :users, only: [:index,:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
-
-end	
+end
