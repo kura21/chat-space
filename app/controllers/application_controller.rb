521258e6a71e4a934e471cff863a06b2270178c0
kana uchimura <xxxxxx@gmail.com>
2019-10-24 14:35:28 +0900

    テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception	
-  # ログイン済みのユーザーのみにアクセス許可
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
-  # deviseで利用可能なパラメータを増やす、device由来のアクションの前に
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
 
-  # deviseのストロングパラメーターをいじる
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-end	
+end
