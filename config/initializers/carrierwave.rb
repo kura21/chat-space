cc14d33c6ebabe1372956920d59640359f23d714
kana uchimura <xxxxxx@gmail.com>
2019-10-26 19:28:48 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'kura21'
-  config.asset_host = 'https://ap-northeast-1.amazonaws.com/kura21'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kura21'
 end
